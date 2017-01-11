5d522c45de2b4f4cb1db69403c30a3bb257507c3
Kaushik Vijayakumar <Kaushik Vijayakumar>
2017-01-11 01:45:31 -0500

    modified

--- Hello World - R Programming.R
+++ Hello World - R Programming.R
@@ -1,34 +1,13 @@
+###########################################################################################
+                      ########### HELLO WORLD - ###########
+                ################ R PROGRAMMING #################
+###########################################################################################
 
-############## Misplaced diamonds dataset - Data inference - START ###############
+Print("hello world")
 
-#Misplaced diamonds dataset - drawing some fascinating inferences from the data and learn on the way!
-
-#choosing the file on the go - Am using the first time :)
-diamond_data = read.csv(file.choose())
-
-#building a scatter plot after loading the relevant library 
-library(ggplot2)
-
-# first build a simple plot 
-ggplot(data = diamond_data, aes (x=carat, y = price))+
-  geom_point() 
-  
-# Upgraded plot
-# Make some cosmetic changes for a good view! Looks better  
-# alpha changes the size of the points in scatter plot
-ggplot(data = diamond_data [diamond_data$carat<2.5,], aes (x=carat, y = price, col = clarity))+
-  geom_point(alpha = .1) +
-  geom_smooth(alpha = .1) #+
-  # geom_line() # bad view as there is too much of intereferences!
-
-
-# Inference: 
-# There are some intewining of the lines which means that the pricing is definitely
-# misplaced. Althought it seems like a clear procing between .7-.7carot range
-
-############## Misplaced diamonds dataset - Data inference - END ###############
-
-
+a = 'hello'
+b = 'world'
+cat(a,b)
 
 ############### Play with Logical operators - START #######################
 a = 4
@@ -119,31 +98,3 @@ answer = counter/N
 answer
 
 ################### LOOPING - END ###################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##################### Rough area #############################
-?rnorm
-norm = rnorm (100)
-norm
-typeof(norm)
-ggplot(data = norm, aes(x = norm)) +
-  geom_histogram(binwidth = 1)
-
-
-
-
-
-
